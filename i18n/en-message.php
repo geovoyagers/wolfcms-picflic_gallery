4178d6ab62be0599b9711617510f8440c0c4641e
seven <seven@geovoyagers.de>
2013-08-18 01:28:37 +0200

    Create en-message.php

---
+++ i18n/en-message.php
@@ -0,0 +1,80 @@
+<?php
+
+/**
+* English language for picasa / flickr online gallery.
+* Translation version 1.0.1
+*
+* @package Plugins
+* @subpackage picflic_gallery
+*
+* @author Tina Keil <seven@geovoyagers.de>
+* @version Wolf 0.7.7
+*/
+
+return array(
+	'Picflic Gallery' => 'Picflic Gallery',
+	'Lets you integrate picasa albums or flickr photosets' => 'Lets you integrate picasa albums or flickr photosets',
+	'General settings' => 'General settings',
+	'Use Cache' => 'Use Cache',
+	'Yes' => 'Yes',
+	'No' => 'No',
+	'Image' => 'Image',
+	'Delete Cache' => 'Delete Cache',
+	'Cache the external xml files?' => 'Cache the external xml files?',
+	'Cache Lifetime' => 'Cache Lifetime',
+	'hours' => 'hours',
+	'Day' => 'Day',
+	'Week' => 'Week',
+	'Month' => 'Month',
+	'How long should the cache files be retained?' => 'How long should the cache files be retained?',
+	'Max. images per page' => 'Max. images per page',
+	'No. of images shown per page' => 'No. of images shown per page',
+	'Max. title length' => 'Max. title length',
+	'Max. string length of title shown below thumbnails' => 'Max. string length of title shown below thumbnails',
+	'Picasa settings' => 'Picasa settings',
+	'Your Picasa Username' => 'Your Picasa Username',
+	'Authkey' => 'Authkey',
+	'Required to access protected albums.' => 'Required to access protected albums.',
+	'longest side' => 'longest side',
+	'Pixel' => 'Pixel',
+	'Large image size' => 'Large image size',
+	'Size of images shown in lightbox' => 'Size of images shown in lightbox',
+	'Thumbnail size' => 'Thumbnail size',
+	'Size of thumbnail in pixel' => 'Size of thumbnail in pixel',
+	'Cropped (square) sizes' => 'Cropped (square) sizes',
+	'Uncropped sizes' => 'Uncropped sizes',
+	'Flickr settings' => 'Flickr settings',
+	'Your Flickr Username' => 'Your Flickr Username',
+	'Api Key' => 'Api Key',
+	'Your Flickr Api Key' => 'Your Flickr Api Key',
+	'Api Secret' => 'Api Secret',
+	'Your Flickr Api Secret' => 'Your Flickr Api Secret',
+	'Api Token' => 'Api Token',
+	'Get token / User ID' => 'Get token / User ID',
+	'Your Flickr Api Token' => 'Your Flickr Api Token',
+	'Api User ID' => 'Api User ID',
+	'Your Flickr User ID' => 'Your Flickr User ID',
+	'Could\'nt save the feed to the cache. Make sure the cache folder is writeable!' => 'Could\'nt save the feed to the cache. Make sure the cache folder is writeable!',
+	'There was a problem calling the feed. Check the album name and permissions or try again later.' => 'There was a problem calling the feed. Check the album name and permissions or try again later.',
+	'Flickr Api Token' => 'Flickr Api Token',
+	'Sorry, you can\'t do this yet.' => 'Sorry, you can\'t do this yet.',
+	'You must first enter the <strong>Flickr Username</strong>' => 'You must first enter the <strong>Flickr Username</strong>',
+	'You must first enter the <strong>Flickr Api Key</strong>' => 'You must first enter the <strong>Flickr Api Key</strong>',
+	'You must first enter the <strong>Flickr Api Secret</strong>' => 'You must first enter the <strong>Flickr Api Secret</strong>',
+	'Please read the' => 'Please read the',
+	'picflic Documentation' => 'picflic Documentation',
+	', if you are not sure how to do this.' => ', if you are not sure how to do this.',
+	'This is your Flickr Api Token' => 'This is your Flickr Api Token',
+	'This is your Flickr User ID' => 'This is your Flickr User ID',
+	'Return to the settings page and enter the token number and User ID to the Flickr Api settings.' => 'Return to the settings page and enter the token number and User ID to the Flickr Api settings.',
+	'Be sure to save the settings.' => 'Be sure to save the settings.',
+	'Step 1: Set callback URL' => 'Step 1: Set callback URL',
+	'Before you can get your Flickr Api Token you must set the callback URL.' => 'Before you can get your Flickr Api Token you must set the callback URL.',
+	'Goto the following page:' => 'Goto the following page:',
+	'There, click on the title of your picflic App which you setup earlier. Then, on the right hand side look for "Edit the authentication flow" and click on the edit link. On that page enter the callback URL, exactly as shown below:' => 'There, click on the title of your picflic App which you setup earlier. Then, on the right hand side look for "Edit the authentication flow" and click on the edit link. On that page enter the callback URL, exactly as shown below:',
+	'Step 2: Get your Flickr Api Token' => 'Step 2: Get your Flickr Api Token',
+	'After you have saved the callback URL, click on the following link to get your Api Token:' => 'After you have saved the callback URL, click on the following link to get your Api Token:',
+	'Get your Token now' => 'Get your Token now',
+	'The cache has been deleted.' => 'The cache has been deleted.',
+	'An error occured trying to delete the cache.' => 'An error occured trying to delete the cache.'
+);
